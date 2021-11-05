94248fbaa4e8676aa1143bb585ba51cb0f849b46
psyk3r <86204745+psyk3r@users.noreply.github.com>
2021-11-05 12:13:34 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,1 @@
-sudo apt-get install -y libasound2-dev
-python3 -m pip install beepy
-python3 -m pip install time
+sudo apt-get install -y libasound2-dev && python3 -m pip install beepy && python3 -m pip install time
